91448bbb1a26ed7976c97187d8dcf1e3936679df
zhenghaoyang24 <2463193508@qq.ocm>
2024-07-19 22:12:08 +0800

    :update config-json

--- vue.config.js
+++ vue.config.js
@@ -17,14 +17,5 @@ module.exports = defineConfig({
                 raw: true,
             });
     },
-    pwa: {
-        iconPaths: {
-            favicon32: 'favicon.ico',
-            favicon16: 'favicon.ico',
-            appleTouchIcon: 'favicon.ico',
-            maskIcon: 'favicon.ico',
-            msTileImage: 'favicon.ico'
-        }
-    },
 
 })
